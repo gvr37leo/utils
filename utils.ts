1e07b3ab8de6dea5605e99724b55b3722bbc3d22
Paul Boon <gvr37.p.l.boon@gmail.com>
2019-10-10 15:36:50 +0200

    p

--- utils.ts
+++ utils.ts
@@ -2,7 +2,7 @@
 
 var TAU = Math.PI * 2
 function map(val:number,from1:number,from2:number,to1:number,to2:number):number{
-    return lerp(inverseLerp(val,from1,from2),to1,to2)
+    return lerp(to1,to2,inverseLerp(val,from1,from2))
 }
 
 function inverseLerp(val:number,a:number,b:number):number{
@@ -403,14 +403,26 @@ class Rule{
 class Ability{
     // ammo:number = 1
     // maxammo:number = 1
+    // ammorechargerate:number = 1000
+    // casttime:number = 2000
+    // channelduration:number = 3000
 
     cooldown:number = 1000
     lastfire = Date.now()
     rules:Rule[] = [
-        new Rule('not ready yet',() => (this.lastfire + this.cooldown) < Date.now())
+        new Rule('not ready yet',() => (this.lastfire + this.cooldown) < Date.now()),
+        //cast while moving rule
+        //must have target rule
+        //must have valid target rule
+        //resource rule
+        //ammo rule
+        //line of sight rule
     ]
-
+    stopwatch:StopWatch = new StopWatch()
+    ventingtime:number = 0
     onCastFinished = new EventSystemVoid()
+    shots: number = 0
+    firing: boolean = false
 
     
 
@@ -418,13 +430,57 @@ class Ability{
 
     }
 
+    //positive if you need to wait 0 or negative if you can call it
+    timeTillNextPossibleActivation():number{
+        return to(Date.now(), this.lastfire + this.cooldown)
+    }
 
-    tryfire(){
-        if(this.rules.some(r => r.cb())){
-            this.cb()
-            this.lastfire = Date.now()
+    canActivate(){
+        return this.rules.some(r => r.cb())
+    }
+
+    callActivate(){
+        this.cb()
+    }
+
+    fire(){//activate
+        if(this.firing == false){
+            this.startfire()
+        }else{
+            this.holdfire()
         }
-            
+    }
+
+    releasefire(){
+        this.firing = false
+    }
+
+    tapfire(){
+        this.startfire()
+        this.releasefire()
+    }
+    
+    private startfire(){
+        if(this.rules.some(r => r.cb())){
+            this.firing = true
+            this.ventingtime = 0
+            this.stopwatch.start()
+            this.ventingtime -= this.cooldown
+            this.shots = 1
+            this.lastfire = Date.now()
+            this.cb()
+        }
+    }
+
+    private holdfire(){
+        this.ventingtime += this.stopwatch.get()
+        this.stopwatch.start()
+        this.shots = Math.ceil(this.ventingtime / this.cooldown)
+        this.ventingtime -= this.shots * this.cooldown
+        this.lastfire = Date.now()
+        if(this.shots > 0){
+            this.cb()
+        }
     }
 }
 
@@ -435,9 +491,6 @@ class Anim{
     reverse:boolean = false
     duration:number = 1000
     stopwatch:StopWatch = new StopWatch()
-
-
-    path:Vector[] = []
     begin:number = 0
     end:number = 1
 
@@ -467,4 +520,199 @@ class Anim{
                 return Math.floor(cycles) * distPerCycle + lerp(this.begin,this.end,mod(cycles,1))
         }
     }
+}
+
+enum Hor {left,middle,right}
+enum Vert {bottom,middle,top}
+function createVector(x:Hor,y:Vert):Vector{
+    return new Vector(x * 0.5,y * 0.5)
+}
+var zero = new Vector(0,0)
+var one = new Vector(1,1)
+var botleft = createVector(Hor.left,Vert.bottom)
+var botmiddle = createVector(Hor.middle,Vert.bottom)
+var botright = createVector(Hor.right,Vert.bottom)
+var middleleft = createVector(Hor.left,Vert.middle)
+var center = createVector(Hor.middle,Vert.middle)
+var middleright = createVector(Hor.right,Vert.middle)
+var topleft = createVector(Hor.left,Vert.top)
+var topmiddle = createVector(Hor.middle,Vert.top)
+var topright = createVector(Hor.right,Vert.top)
+
+
+class BezierAnim extends Anim{
+    private path:Vector[] = []
+
+    constructor(public controlPoints:Vector[]){
+        super()
+        this.cacheControlPoints()
+    }
+
+    cacheControlPoints(){
+        var precision = 11
+        this.path = Bezier.cacheSlopeX(precision,Bezier.computeWaypointsContinuously(precision,this.controlPoints))
+    }
+
+    getSmooth(){
+        return Bezier.tween(this.get(),this.path).y
+    }
+    static in = [botleft,botmiddle]
+    static out = [topmiddle,topright]
+
+    static linear = [botleft,center,center,topright]
+    static easeInEaseOut = [...BezierAnim.in,...BezierAnim.out]
+    static easeOut = [botleft,center,...BezierAnim.out]
+    static easeIn = [...BezierAnim.in,center,topright]
+}
+
+
+
+class Bezier{
+    constructor(){
+
+    }
+
+    static getBezierPoint(t:number,p0:Vector,p1:Vector,p2:Vector,p3:Vector):Vector{
+        var a = p0.lerp(p1,t)
+        var b = p1.lerp(p2,t)
+        var c = p2.lerp(p3,t)
+        var d = a.lerp(b,t)
+        var e = b.lerp(c,t)
+        var res = d.lerp(e,t)
+        return res
+    }
+
+    static computeWaypoints(numberOfWaypoints:number,p0:Vector,p1:Vector,p2:Vector,p3:Vector){
+        var spaces = numberOfWaypoints - 1
+        var waypoints:Vector[] = [];
+        for(var i = 0; i < numberOfWaypoints; i++){
+            waypoints.push(Bezier.getBezierPoint(i / spaces, p0,p1,p2,p3))
+        }
+        return waypoints;
+    }
+
+    static computeWaypointsContinuously(waypointsPerSection:number,controlpoints:Vector[]):Vector[]{
+        var waypoints:Vector[] = Bezier.computeWaypoints(10,controlpoints[0],controlpoints[1],controlpoints[2],controlpoints[3])
+        for(var i = 3; i < controlpoints.length - 3; i += 3){
+            var result = Bezier.computeWaypoints(waypointsPerSection,controlpoints[i],controlpoints[i + 1],controlpoints[i + 2],controlpoints[i + 3])
+            result.shift()
+            waypoints = waypoints.concat(result)
+        }
+        return waypoints
+    }
+
+    static calcLength(waypoints:Vector[]){
+        var length = 0;
+        for(var i = 1; i < waypoints.length; i++){
+            length += waypoints[i].to(waypoints[i - 1]).length()
+        }
+        return length;
+    }
+
+    static tween(t:number, waypoints:Vector[]){
+        var lm1 = waypoints.length - 1;
+        var low = Math.floor(lm1 * t)
+        var high = Math.ceil(lm1 * t)
+        return waypoints[low].lerp(waypoints[high],t*lm1 - Math.floor(t*lm1))
+    }
+
+    static constantDistanceWaypoints(waypoints:Vector[],numberOfWaypoints:number){
+        var length = this.calcLength(waypoints);
+        var spacing = length / (numberOfWaypoints - 1)
+        var result:Vector[] = [first(waypoints).c()]
+        
+        var budget = 0
+        for(var i = 0; i < waypoints.length - 1; i++){
+            var a = waypoints[i]
+            var b = waypoints[i + 1]
+            var length = a.to(b).length()
+            var remainingLength = budget
+            budget += length
+            var fits = Math.floor((remainingLength + length) / spacing) 
+            budget -= fits * spacing
+            for(var j = 1; j <= fits; j++){
+                result.push(a.lerp(b,(j * spacing - remainingLength) / length))
+            }
+        }
+        result.push(last(waypoints).c())
+        return result
+    }
+
+    //points need to be guaranteed left to tight
+    static cacheSlopeX(samplePoints:number,points:Vector[]):Vector[]{
+        var result = []
+        var spaces = samplePoints - 1;
+        for(var i = 0; i < samplePoints; i++){
+            result.push(new Vector(lerp(first(points).x, last(points).x, i / spaces), 0))
+        }
+        var sectionIndex = 0
+        for(var point of result){
+            var a = points[sectionIndex]
+            var b = points[sectionIndex + 1]
+            while(!inRange(a.x,b.x,point.x)){
+                sectionIndex++
+                a = points[sectionIndex]
+                b = points[sectionIndex + 1]
+            }
+            point.y = map(point.x,a.x,b.x,a.y,b.y)
+        }
+        return result
+    }
+}
+
+function first<T>(arr:T[]):T{
+    return arr[0]
+}
+
+function cacheSin(precision:number){
+    for(var i = 0; i < precision;i++){
+        sinCache[i] = Math.sin((i / precision) * TAU)
+    }
+}
+
+var sinCache = []
+cacheSin(360)
+
+function sinCached(radians:number){
+    var percentage = mod(radians,TAU) / TAU
+    var abs = percentage * sinCache.length
+    var bot = Math.floor(abs)
+    var top = Math.ceil(abs)
+    var remains = abs - bot
+    return lerp(sinCache[bot],sinCache[top % sinCache.length],remains)
+}
+
+function cacheCos(precision:number){
+    for(var i = 0; i < precision;i++){
+        cosCache[i] = Math.cos((i / precision) * TAU)
+    }
+}
+
+var cosCache = []
+cacheCos(360)
+
+function cosCached(radians:number){
+    var percentage = mod(radians,TAU) / TAU
+    var abs = percentage * cosCache.length
+    var bot = Math.floor(abs)
+    var top = Math.ceil(abs)
+    var remains = abs - bot
+    return lerp(cosCache[bot],cosCache[top % cosCache.length],remains)
+}
+
+function rotate2d(v:Vector,rotations:number){
+    var s = sinCached(rotations * TAU)
+    var c = cosCached(rotations * TAU)
+    var x = v.x * c - v.y * s
+    var y = v.x * s + v.y * c
+    v.x = x
+    v.y = y
+    return v
+}
+
+function rotate2dCenter(v:Vector,rotations:number,center:Vector){
+    v.sub(center)
+    rotate2d(v,rotations)
+    v.add(center)
+    return v
 }
